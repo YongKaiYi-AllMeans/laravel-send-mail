f1517424171d83d637e12d0fcb69540fce8c1873
Yong Kai Yi <kaiyi@allmeans.com.sg>
2022-09-05 12:00:44 +0800

    mail setup

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/sendmail', [\App\Http\Controllers\SendmailController::class, 'sendMail']);
